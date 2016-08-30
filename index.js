db48e0105c6b0544c4089dee0d052c3106e8cfe1
Eduardo Gonzalez <eduardo.gch@gmail.com>
2016-08-30 14:44:09 -0400

    Add files for log server

---
+++ index.js
@@ -0,0 +1,39 @@
+var FileStreamRotator = require('file-stream-rotator');
+var express = require('express');
+var fs = require('fs');
+var morgan = require('morgan');
+var path = require('path');
+var logDirectory = path.join(__dirname, 'logs');
+
+if (!fs.existsSync(logDirectory)) {
+	fs.mkdirSync(logDirectory);
+}
+
+var accessLogStream = FileStreamRotator.getStream({
+	date_format: 'YYYYMMDD',
+	filename: path.join(logDirectory, 'access-%DATE%.log'),
+	frequency: 'daily',
+	verbose: false
+});
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('common', {
+	stream: accessLogStream
+}));
+
+app.get('/', function(req, res) {
+	res.send('Good!');
+});
+
+app.all('*', function(req, res) {
+	res.redirect('/');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Listening on port %d', app.get('port'));
+});
+
+module.exports = app;
